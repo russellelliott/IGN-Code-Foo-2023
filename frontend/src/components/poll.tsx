588e901eab318798b773c42d63dd8219811795f8
Russell Elliott <russell@quadsquad.net>
2023-03-29 12:13:01 -0700

    display options

--- frontend/src/components/poll.tsx
+++ frontend/src/components/poll.tsx
@@ -2,8 +2,17 @@ import * as React from 'react';
 import {Poll} from '../types/poll/index';
 import { useEffect, useState } from 'react';
 
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+
 
 function Poll() {
+    function select(poll: Poll){
+        //setAccount(account);
+        console.log(poll.id);
+        //alert(ID);
+    }
     const [options, setOptions] = useState<Poll[]>([]);
 
     useEffect(() => {
@@ -35,7 +44,20 @@ function Poll() {
       },[]);
       return (
         <React.Fragment>
-            hello world
+            What is your favorite Sci-Fi Media?
+            {options.map(option => {
+            //console.log(user);
+            return (
+              <tr key={option.id}>
+                <td>{option.data}</td>
+                <td>
+                  <IconButton id = {"edit"+option.id} aria-label={"select "+option.id} size="large" onClick={() => { select(option) }}>
+                    <EditIcon />
+                  </IconButton>
+                </td>
+              </tr>
+            );
+          })}
             
         </React.Fragment>
       );

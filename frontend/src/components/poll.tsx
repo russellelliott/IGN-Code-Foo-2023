a13fb625214e898d23ec9231e052dcd191da41bb
Russell Elliott <russell@quadsquad.net>
2023-03-29 13:08:59 -0700

    removing errors messes with vote display

--- frontend/src/components/poll.tsx
+++ frontend/src/components/poll.tsx
@@ -38,11 +38,11 @@ function Poll() {
               //console.log(json.data.getAllAccounts);
             })
           }
-        //   sendData().catch((err) => {
-        //     console.log(err);
-        //     //Router.push({pathname: '/error'});
-        //     return;
-        //   });
+          sendData().catch((err) => {
+            console.log(err);
+            //Router.push({pathname: '/error'});
+            return;
+          });
 
           const fetchData = async () => {
             const query = {query: `query Poll {
@@ -64,11 +64,11 @@ function Poll() {
               //console.log(json.data.getAllAccounts);
             })
           }
-        //   fetchData().catch((err) => {
-        //     console.log(err);
-        //     //Router.push({pathname: '/error'});
-        //     return;
-        //   });
+          fetchData().catch((err) => {
+            console.log(err);
+            //Router.push({pathname: '/error'});
+            return;
+          });
     }
     const [options, setOptions] = useState<Poll[]>([]);
     const [vote, setVote] = useState<Poll>();
